286e36bed1c3fb3726bb2880bd52ef7fe1a51052
fran8ctba <fran8ctba@hotmail.com>
2023-10-15 11:07:47 -0300

    terceira versão

---
+++ carrossel.js
@@ -0,0 +1,24 @@
+let radio = document.querySelector('.manual-btn')/*aqui chama a navegação manual*/ 
+let cont = 1
+
+document.getElementById ('radio1').checked = true/*aqui define o item como verdade,ou seja,sele vai ficar marcado */
+
+setInterval(() => {
+    proximaImg()
+}, 5000)/*aqui é para passar a imagem depois de um tempo.  */
+
+function proximaImg(){
+    cont++
+
+    if(cont > 5){
+        cont = 1
+    }
+
+    document.getElementById ('radio'+ cont).checked = true
+}
+
+
+
+function topo() {/*função para quando clicado no rodape ele volte para o topo da pagina*/ 
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
